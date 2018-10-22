dec008c4fa68a710e11743ee915a1293e0d0a458
lostkid456 <jzhao7688@bths.edu>
2018-10-22 15:32:58 -0400

    Lol

---
+++ src/Classroom.java
@@ -0,0 +1,30 @@
+public class Classroom
+{
+    private Student[] students;
+    private Teacher teacher;
+
+    public Classroom(Teacher teacher, Student[] students)
+    {
+        this.students=students;
+        this.teacher=teacher;
+    }
+
+    public String getSubject();
+    public double classAverage()
+    {
+        int x=0;
+        for(int i=0;i<students.length;i++)
+        {
+            x+=students[i].getGPA();
+        }
+        return x/students.length;
+    }
+    public String printClass();
+
+    public String toString()
+    {
+        return teacher+","+students;
+    }
+
+
+}
