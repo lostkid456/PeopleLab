b3b9a0cea07304250192ff98b5a3ac190d00898d
ByronChen <bchen9694@bths.edu>
2018-10-23 14:48:42 -0400

    teacher

---
+++ src/Teacher.java
@@ -0,0 +1,17 @@
+public class Teacher extends Person{
+
+    private String Subject;
+    private String Title;
+
+    public Teacher(String firstName,String familyName, String Title, String Subject){
+        super(firstName, familyName);
+        this.Subject = Subject;
+        this.Title = Title;
+    }
+    public String getSubject() { return(Subject); }
+    public String toString() {
+       // int i = (int)(Math.random() * 4 + 1);
+        return(Title + ". " + this.getFamilyName());
+    }
+
+}
